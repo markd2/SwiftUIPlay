2a78812f0f88735b9fb30413edf2ec8295e8a796
MarkD <markd@borkware.com>
2021-03-06 10:17:28 -0500

    Plumbs remote into ContentView.

--- TISch2/TISch2/ContentView.swift
+++ TISch2/TISch2/ContentView.swift
@@ -1,14 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var remote: PhotoRemote
+
+    init(remote: PhotoRemote) {
+        self.remote = remote
+    }
+
     var body: some View {
         Text("Splunge")
             .padding()
+        Text("Blargle \(remote.loadedData?.count ?? -666)")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(remote: PhotoRemote())
     }
 }
