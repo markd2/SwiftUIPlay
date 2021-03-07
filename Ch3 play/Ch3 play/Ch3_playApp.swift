f219157082fc4d6ba74afe11f8619f058a741dfb
MarkD <markd@borkware.com>
2021-03-07 13:02:54 -0500

    new project from template, for chapter 3 of TiS

---
+++ Ch3 play/Ch3 play/Ch3_playApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ch3_playApp.swift
+//  Ch3 play
+//
+//  Created by Mark Dalrymple on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Ch3_playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ch3 play/Ch3 play/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ch3 play
+//
+//  Created by Mark Dalrymple on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
