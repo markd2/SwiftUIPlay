730280447efe8fafca2e2dcc813d6371451e6487
MarkD <markd@borkware.com>
2021-03-21 12:48:56 -0400

    Got the UI mostly working (if ugly) But the UX is pretty terrible.  The one and only text field does't focus, and after adding an item, doesn't refocus.

--- Choosinator/Choosinator/ContentView.swift
+++ Choosinator/Choosinator/ContentView.swift
@@ -1,16 +1,33 @@
-//
-//  ContentView.swift
-//  Choosinator
-//
-//  Created by Mark Dalrymple on 3/21/21.
-//
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var newItem = ""
+    @State private var items: [String] = ["Greeble", "Bork"]
+    @State private var randomItem = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Choosinator")
+              .padding()
+            TextField("Something to do", text: $newItem, 
+                      onCommit: {
+                          items += [newItem]
+                          newItem = ""
+                          // ??? How to re-focus text field when adding an item
+                          UIApplication.shared.becomeFirstResponder()
+                      })
+            Button("Choose one") {
+                randomItem = items.randomElement() ?? ""
+            }
+            Text(randomItem)
+            List {
+                ForEach(items, id: \.self) { item in
+                    Text(item)
+                }
+            }
+        }
     }
 }
 
