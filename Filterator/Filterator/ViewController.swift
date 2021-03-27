79af916225a976500b30778d5121a63ee2e4e417
MarkD <markd@borkware.com>
2021-03-27 08:56:48 -0400

    new template project

---
+++ Filterator/Filterator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filterator
+//
+//  Created by Mark Dalrymple on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
