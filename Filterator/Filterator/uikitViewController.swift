ba02860600ef4c2d990c4132f8d36165866af5d9
MarkD <markd@borkware.com>
2021-03-27 12:03:16 -0400

    Renames the generic UIKit view controller

--- Filterator/Filterator/uikitViewController.swift
+++ Filterator/Filterator/uikitViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class uikitViewController: UIViewController {
     @IBOutlet var editorContainer: UIStackView!
 
     @IBOutlet var personTableView: UITableView!
