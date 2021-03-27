642149c7dee128e61bfb3482e974043fc2176652
MarkD <markd@borkware.com>
2021-03-27 10:17:44 -0400

    Displays names in tableview

--- Filterator/Filterator/ViewController.swift
+++ Filterator/Filterator/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var editorContainer: UIStackView!
+
+    @IBOutlet var personTableView: UITableView!
+    var personDataSource: UITableViewDiffableDataSource<Int, Person>!
+
+    var everyone: [Person] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("\(Person.random(count: 10000))")
+
+        everyone = Person.random(count: 500)
+
+        personTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Splunge") 
+
+        personDataSource = UITableViewDiffableDataSource(tableView: personTableView) {
+            (tableView, indexPath, person) in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Splunge", for: indexPath)
+            cell.textLabel?.text = person.name + " type " + person.bloodType.rawValue + " size \(person.shoeSize)"
+            return cell
+        }
+        
+        var snapshot = personDataSource.snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(everyone, toSection: 0)
+        personDataSource.apply(snapshot, animatingDifferences: false)
     }
 }
 
