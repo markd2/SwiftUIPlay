c55be4a4b5e9ef8bb095d7aed51fddd84bec6801
MarkD <markd@borkware.com>
2021-03-27 20:28:56 -0400

    Responds to filter changes (doesn't update UI yet)

--- Filterator/Filterator/DataGod.swift
+++ Filterator/Filterator/DataGod.swift
@@ -18,6 +18,28 @@ class DataGod {
         results = everyone
     }
 
+    func applyFilters(_ filters: Filters) {
+        let filtered = everyone.filter { person in
+            var allow = true
+
+            if let bloodType = filters.bloodType {
+                if person.bloodType != bloodType {
+                    allow = false
+                }
+            }
+
+            if let name = filters.name {
+                if let _ = person.name.range(of: name, options: .caseInsensitive) {
+                } else {
+                    allow = false
+                }
+            }
+
+            return allow
+        }
+        results = filtered
+    }
+
     init(everyone: [Person]) {
         self.everyone = everyone
         self.results = everyone
@@ -25,7 +47,7 @@ class DataGod {
                                           print("COMPLETED \(status)")
                                       },
                                       receiveValue: { filters in
-                                          print("FILTERS \(filters)")
+                                          self.applyFilters(filters)
                                       })
 
     }
