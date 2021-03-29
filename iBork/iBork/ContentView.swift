f392717c69874a7b2e96cf78d5986137fa602691
MarkD <markd@borkware.com>
2021-03-28 20:56:12 -0400

    New project from template

---
+++ iBork/iBork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iBork
+//
+//  Created by Mark Dalrymple on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iBork/iBork/iBorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBorkApp.swift
+//  iBork
+//
+//  Created by Mark Dalrymple on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct iBorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
