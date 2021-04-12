d4b459d3b34ecfff307ff9bf37fd49ddb758abb6
Mark Dalrymple <markd@borkware.com>
2021-04-12 17:30:52 -0400

    Extends sequence to have running stuff

--- Yoginator/Yoginator/Sequence.swift
+++ Yoginator/Yoginator/Sequence.swift
@@ -2,4 +2,21 @@ import Foundation
 
 struct Sequence {
     let poses: [RunPose]
+    let isRunning: Bool
+    let currentTime: Int
+}
+
+
+extension Sequence {
+    static var hardcoded: Sequence {
+        let poses = [
+          Pose(name: "Scoobydooasana", imageName: "", defaultDuration: 30),
+          Pose(name: "Radioasana", imageName: "", defaultDuration: 15),
+          Pose(name: "Greebleborkenasana", imageName: "", defaultDuration: 10)
+        ]
+
+        let runPoses = poses.map { RunPose(pose: $0, duration: $0.defaultDuration) }
+
+        return Sequence(poses: runPoses, isRunning: false, currentTime: 0)
+    }
 }
