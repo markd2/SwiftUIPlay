86f80567af8ad28c961bad88ab16a06e6ffef82b
Mark Dalrymple <markd@borkware.com>
2022-03-09 20:40:22 -0500

    wesplitte from template

---
+++ 100days/WeSplitte/WeSplitte/WeSplitteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitteApp.swift
+//  WeSplitte
+//
+//  Created by markd on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
