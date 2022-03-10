c1b3ba70b79e4d6f9206224bf1b95241e5dc27b7
Mark Dalrymple <markd@borkware.com>
2022-03-10 00:41:40 -0500

    factor currency

--- 100days/WeSplitte/WeSplitte/ContentView.swift
+++ 100days/WeSplitte/WeSplitte/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
+    
+    let currency = FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currencyCode ?? "USD")
 
     let tipPercentages = [10, 15, 20, 25, 0]
 
@@ -33,8 +35,7 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amoont", value: $checkAmount, 
-                              format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Amoont", value: $checkAmount, format: currency)
                       .keyboardType(.decimalPad)
                       .focused($amountIsFocused)
                     
@@ -48,17 +49,15 @@ struct ContentView: View {
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< 101) {
-                            Text(\($0 %), format: .percent)
+                            Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tippage?")
                 }
 
                 Section {
-                    Text(totalPerPerson, 
-                         format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(totalPerPerson, format: currency)
                 } header: {
                     Text("Amount per peep")
                 }
