246221635e6141892246c7813e2151e98ea0264c
Mark Dalrymple <markd@borkware.com>
2022-03-11 17:56:46 -0500

    checkpoint

--- 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
+++ 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
-                     "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
+                                    "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
 
-    var body: some View {
-        VStack {
-            Text("Tappy the flag of")
-            Text(countries[correctAnswer])
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Very Incorrect"
         }
+        showingScore = true
     }
 
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+        
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]),
+                           startPoint: .top, endPoint: .bottom)
+              .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tappy the flag of")
+                      .font(.subheadline.weight(.heavy))
+                      .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                      .font(.largeTitle.weight(.semibold))
+                      .foregroundColor(.white)
+                }
+                VStack {
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number]).renderingMode(.original)
+                        }
+                    }
+                }
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is _wat_")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
