1865a798dc773bcaa088e9d5902fefb5878fb4e8
Mark Dalrymple <markd@borkware.com>
2022-03-11 15:57:22 -0500

    adds flags

--- 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
+++ 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
+                     "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Tappy the flag of")
+            Text(countries[correctAnswer])
+        }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
