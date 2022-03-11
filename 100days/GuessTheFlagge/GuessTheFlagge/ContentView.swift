6bd53fe2269bb4a2e39256cb2538db82df846b69
Mark Dalrymple <markd@borkware.com>
2022-03-11 18:08:47 -0500

    checkpoint

--- 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
+++ 100days/GuessTheFlagge/GuessTheFlagge/ContentView.swift
@@ -31,30 +31,43 @@ struct ContentView: View {
         
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]),
-                           startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                             .init(color: Color(red: 0.1, green: 0.2, blue: 0.45),  location: 0.3),
+                             .init(color: Color(red: 0.76, green: 0.15, blue: 0.26),  location: 0.3),
+                           ], center: .top, startRadius: 200, endRadius: 400)
               .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tappy the flag of")
-                      .font(.subheadline.weight(.heavy))
-                      .foregroundColor(.white)
-                    Text(countries[correctAnswer])
-                      .font(.largeTitle.weight(.semibold))
-                      .foregroundColor(.white)
-                }
-                VStack {
-                    ForEach(0..<3) { number in
-                        Button {
-                            flagTapped(number)
-                        } label: {
-                            Image(countries[number])
-                              .renderingMode(.original)
-                              .clipShape(Capsule())
-                              .shadow(radius: 5)
+
+            VStack {
+                Text("Guess the Flag")
+                  .font(.largeTitle.weight(.bold))
+                  .foregroundColor(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tappy the flag of")
+                          .font(.subheadline.weight(.heavy))
+                          .foregroundColor(.white)
+                        Text(countries[correctAnswer])
+                          .font(.largeTitle.weight(.semibold))
+                          .foregroundColor(.white)
+                    }
+                    VStack {
+                        ForEach(0..<3) { number in
+                            Button {
+                                flagTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                  .renderingMode(.original)
+                                  .clipShape(Capsule())
+                                  .shadow(radius: 5)
+                            }
                         }
                     }
                 }
+                  .frame(maxWidth: .infinity)
+                  .padding(.vertical, 20)
+                  .background(.regularMaterial)
+                  .clipShape(RoundedRectangle(cornerRadius: 20))
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
