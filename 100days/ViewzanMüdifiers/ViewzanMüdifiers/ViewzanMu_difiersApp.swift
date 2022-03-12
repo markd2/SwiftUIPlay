2d01d37f26b0fae7cdb0ba40ddf92daf947a2aae
Mark Dalrymple <markd@borkware.com>
2022-03-11 21:33:47 -0500

    project 3 template

---
+++ 100days/ViewzanMüdifiers/ViewzanMüdifiers/ViewzanMu_difiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewzanMu_difiersApp.swift
+//  ViewzanMüdifiers
+//
+//  Created by markd on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewzanMu_difiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
