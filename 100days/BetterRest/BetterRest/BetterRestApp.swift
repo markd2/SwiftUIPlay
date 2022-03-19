54704e457da6bb1633c48ecb8517f4a18443076e
Mark Dalrymple <markd@borkware.com>
2022-03-19 19:06:17 -0400

    New project from template

---
+++ 100days/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by markd on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
