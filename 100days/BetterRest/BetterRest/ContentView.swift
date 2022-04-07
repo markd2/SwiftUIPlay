50616cf67cc04de11480166e459ef0d8eca1a7ea
Mark Dalrymple <markd@borkware.com>
2022-04-06 22:13:32 -0400

    checkpoint date stuffs

--- 100days/BetterRest/BetterRest/ContentView.swift
+++ 100days/BetterRest/BetterRest/ContentView.swift
@@ -11,12 +11,38 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
 
+    func dateSplunge() -> Date {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 2
+
+        let date = Calendar.current.date(from: components)!
+        return date
+    }
+
+    func dateSplungeTwo() {
+        let someDate = Date()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
+        _ = hour
+        _ = minute
+    }
+
+    init() {
+        wakeUp = dateSplunge()
+        print("\(wakeUp)")
+    }
+
     var body: some View {
         VStack {
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             DatePicker("Please enter a date", selection: $wakeUp,
                        in: Date.now...)
-            .labelsHidden() // gives label to accessibility
+              .labelsHidden() // gives label to accessibility
+            Text("Splunge")
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
     }
 }
