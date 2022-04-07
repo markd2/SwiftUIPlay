bc99db4c452d9d2d81a84c96bc4c34873d6c3b8a
Mark Dalrymple <markd@borkware.com>
2022-04-06 22:52:44 -0400

    checkpoint betterestUI jazz

--- 100days/BetterRest/BetterRest/ContentView.swift
+++ 100days/BetterRest/BetterRest/ContentView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     func dateSplunge() -> Date {
         var components = DateComponents()
         components.hour = 8
         components.minute = 2
-
+        
         let date = Calendar.current.date(from: components)!
         return date
     }
-
+    
     func dateSplungeTwo() {
         let someDate = Date()
         let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
@@ -29,20 +30,42 @@ struct ContentView: View {
         _ = hour
         _ = minute
     }
-
+    
     init() {
         wakeUp = dateSplunge()
         print("\(wakeUp)")
     }
-
+    
+    func calculateBedtime() {
+        
+    }
+    
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp,
-                       in: Date.now...)
-              .labelsHidden() // gives label to accessibility
-            Text("Splunge")
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                  .font(.headline)
+                
+                DatePicker("Please enter a time",
+                           selection: $wakeUp,
+                           displayedComponents: .hourAndMinute)
+                  .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                  .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                  .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                        value: $coffeeAmount, in: 1...20)
+            }
+              .navigationTitle("BetterRest")
+              .toolbar {
+                  Button("Calculate", action: calculateBedtime)
+              }
         }
     }
 }
