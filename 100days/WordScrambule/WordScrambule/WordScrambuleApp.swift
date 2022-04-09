7b900d806d97a22b0aad6ec1a83161a5c298d2d3
Mark Dalrymple <markd@borkware.com>
2022-04-09 19:42:42 -0400

    new project from template

---
+++ 100days/WordScrambule/WordScrambule/WordScrambuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambuleApp.swift
+//  WordScrambule
+//
+//  Created by markd on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
