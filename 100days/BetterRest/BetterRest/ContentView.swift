93968d338d1e5f36adfb7b615a9a81b6d3ddd0ba
Mark Dalrymple <markd@borkware.com>
2022-04-09 16:11:49 -0400

    checkpoint

--- 100days/BetterRest/BetterRest/ContentView.swift
+++ 100days/BetterRest/BetterRest/ContentView.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+
     func dateSplunge() -> Date {
         var components = DateComponents()
         components.hour = 8
@@ -37,7 +43,29 @@ struct ContentView: View {
     }
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute],
+                                                             from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+
+        showingAlert = true
     }
     
     var body: some View {
@@ -66,6 +94,11 @@ struct ContentView: View {
               .toolbar {
                   Button("Calculate", action: calculateBedtime)
               }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
 }
