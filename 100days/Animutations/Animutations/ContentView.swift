e26e0fd490e91f252cac9af0289cc44c75534a1f
Mark Dalrymple <markd@borkware.com>
2022-04-10 19:27:30 -0400

    checkpoint

--- 100days/Animutations/Animutations/ContentView.swift
+++ 100days/Animutations/Animutations/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 0.0
     @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
 
     var body: some View {
         LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
@@ -17,6 +18,17 @@ struct ContentView: View {
                                           endPoint: .bottomTrailing)
             .frame(width: 300, height: 200)
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            // .animation(.spring(), value: dragAmount)
+        .offset(dragAmount)
+        .gesture(
+          DragGesture()
+            .onChanged { dragAmount = $0.translation }
+            .onEnded { _ in 
+                withAnimation(.spring()) {
+                    dragAmount = .zero 
+                }
+            }
+        )
     }
 }
 
