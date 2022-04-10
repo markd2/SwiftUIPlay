3896f49b1f11201158ccff07e985aa7ca4ff5677
Mark Dalrymple <markd@borkware.com>
2022-04-10 19:47:18 -0400

    checkpoint

--- 100days/Animutations/Animutations/ContentView.swift
+++ 100days/Animutations/Animutations/ContentView.swift
@@ -8,30 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Splunge Waffle")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
 
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0 ..< letters.count) { num in
-                Text(String(letters[num]))
-                .padding(5)
-                .font(.title)
-                .background(enabled ? .blue : .red)
-                .offset(dragAmount)
-                .animation(.default.delay(Double(num) / 20),
-                           value: dragAmount)
+        VStack {
+            Button("Bite Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                  .fill(.red)
+                  .frame(width: 200, height: 200)
+                .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-          .gesture(
-            DragGesture()
-              .onChanged { dragAmount = $0.translation }
-              .onEnded { _ in
-                  dragAmount = .zero
-                  enabled.toggle()
-              }
-          )
     }
 }
 
