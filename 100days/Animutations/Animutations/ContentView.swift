a3c2649c04e1af649595fb1abf4c92f6550f164c
Mark Dalrymple <markd@borkware.com>
2022-04-10 19:38:19 -0400

    oooh, snakewaffles

--- 100days/Animutations/Animutations/ContentView.swift
+++ 100days/Animutations/Animutations/ContentView.swift
@@ -8,27 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    let letters = Array("Splunge Waffle")
     @State private var enabled = false
     @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
-                                          startPoint: .topLeading,
-                                          endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            // .animation(.spring(), value: dragAmount)
-        .offset(dragAmount)
-        .gesture(
-          DragGesture()
-            .onChanged { dragAmount = $0.translation }
-            .onEnded { _ in 
-                withAnimation(.spring()) {
-                    dragAmount = .zero 
-                }
+        HStack(spacing: 0) {
+            ForEach(0 ..< letters.count) { num in
+                Text(String(letters[num]))
+                .padding(5)
+                .font(.title)
+                .background(enabled ? .blue : .red)
+                .offset(dragAmount)
+                .animation(.default.delay(Double(num) / 20),
+                           value: dragAmount)
             }
-        )
+        }
+          .gesture(
+            DragGesture()
+              .onChanged { dragAmount = $0.translation }
+              .onEnded { _ in
+                  dragAmount = .zero
+                  enabled.toggle()
+              }
+          )
     }
 }
 
