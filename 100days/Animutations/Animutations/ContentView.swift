831bc8fe677ab0568149641a8cfc7df4ed6288e4
Mark Dalrymple <markd@borkware.com>
2022-04-10 19:12:14 -0400

    that's pretty

--- 100days/Animutations/Animutations/ContentView.swift
+++ 100days/Animutations/Animutations/ContentView.swift
@@ -12,18 +12,11 @@ struct ContentView: View {
     @State private var enabled = false
 
     var body: some View {
-        Button("Bite Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationAmount += 360
-                enabled.toggle()
-            }
-        }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .red : .blue)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
+                                          startPoint: .topLeading,
+                                          endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
