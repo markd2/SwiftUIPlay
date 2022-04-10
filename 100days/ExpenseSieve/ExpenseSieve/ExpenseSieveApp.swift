bc08b5e4c958a34ec1525f5ac939a4085d8fd216
Mark Dalrymple <markd@borkware.com>
2022-04-10 19:59:31 -0400

    new project template

---
+++ 100days/ExpenseSieve/ExpenseSieve/ExpenseSieveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseSieveApp.swift
+//  ExpenseSieve
+//
+//  Created by markd on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseSieveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
