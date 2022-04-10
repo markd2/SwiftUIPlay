ff14a4b5c8e2980e5416cc5d9988b9d2ca0ddb65
Mark Dalrymple <markd@borkware.com>
2022-04-09 21:00:34 -0400

    new project from template

---
+++ 100days/Animutations/Animutations/AnimutationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimutationsApp.swift
+//  Animutations
+//
+//  Created by markd on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimutationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
