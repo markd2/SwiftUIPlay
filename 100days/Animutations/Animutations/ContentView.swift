1b27efe914daaf16f8f18e897be7703c9d9ca517
Mark Dalrymple <markd@borkware.com>
2022-04-09 21:40:56 -0400

    checkpoint

--- 100days/Animutations/Animutations/ContentView.swift
+++ 100days/Animutations/Animutations/ContentView.swift
@@ -11,27 +11,27 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-        Button("Bite Me") {
-            // animationAmount += 0.3
-        }
-        .onAppear {
-            animationAmount = 2
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        // .scaleEffect(animationAmount)
-        // .blur(radius: (animationAmount - 1) * 3)
-        .overlay(
-          Circle()
-            .stroke(.blue)
+        print(animationAmount)
+
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                                      .easeInOut(duration: 1)
+                                        .repeatCount(3, autoreverses: true)
+                                    ),
+                    in: 1...10)
+            Spacer()
+
+            Button("Bite Me") {
+                animationAmount += 1
+            }
+              .padding(40)
+            .background(.purple)
+            .foregroundColor(.white)
+            .clipShape(Circle())
             .scaleEffect(animationAmount)
-            .opacity(2 - animationAmount)
-            .animation(.easeInOut(duration: 2)
-                         .repeatForever(autoreverses: false),
-                       value: animationAmount)
-        )
+
+            Spacer()
+        }
     }
 }
 
