970b3fd42a6b1396295caaa89a25aa77e33cd5b5
Mark Dalrymple <markd@borkware.com>
2022-04-13 10:39:16 -0400

    new munshot from app template

---
+++ 100days/Munshot/Munshot/MunshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunshotApp.swift
+//  Munshot
+//
+//  Created by markd on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MunshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
