42104a17db5741f1c8d58ca99b54b144f2dd0129
Mark Dalrymple <markd@borkware.com>
2022-04-13 10:29:07 -0400

    checkpoit

--- 100days/ExpenseSieve/ExpenseSieve/ContentView.swift
+++ 100days/ExpenseSieve/ExpenseSieve/ContentView.swift
@@ -12,40 +12,66 @@ class User: ObservableObject {
     @Published var lastName = "Bork"
 }
 
-struct ExpenseItem: Identifiable {
-    let id = UUID() // uh...
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID() // uh... x2  Isn't creating this every time in a struct bad? Then hacking it to var because of codable?  todo: put uuidgen into an initializer
     let name: String
     let type: String
     let amount: Double // baby Jesus is crying
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self,
+                                                            from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
 
 
 struct ContentView: View {
     @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name).font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal",
-                                              amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 } label: {
                     Image(systemName: "tortoise")
                 }
             }
         }
         .navigationTitle("Splunge")
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
     }
 
     func removeItems(at offsets: IndexSet) {
@@ -56,5 +82,44 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "impersonal"
+    @State private var amount = 0.0
+
+    @ObservedObject var expenses: Expenses
+
+    @Environment(\.dismiss) var dismiss
+
+    let types = ["bidness", "impersonal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount,
+                          format: .currency(code: "USD"))
+                  .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item) // ewww
+                    dismiss()
+                }
+            }
+        }
     }
 }
