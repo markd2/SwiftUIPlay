88e24aaca53273d76886ddd66774abe6d36b6695
MarkD <markd@borkware.com>
2023-12-19 18:06:25 -0500

    new app from template

---
+++ visualGuide/Viz/Viz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Viz
+//
+//  Created by Mark Dalrymple on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ visualGuide/Viz/Viz/VizApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizApp.swift
+//  Viz
+//
+//  Created by Mark Dalrymple on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct VizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
