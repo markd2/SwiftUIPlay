8cad2ee30058850dc22ca736c3fdb404002a955a
MarkD <markd@borkware.com>
2023-12-21 09:30:13 -0500

    adds blank template

---
+++ suif/suif/suif/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  suif
+//
+//  Created by Mark Dalrymple on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ suif/suif/suif/suifApp.swift
@@ -0,0 +1,17 @@
+//
+//  suifApp.swift
+//  suif
+//
+//  Created by Mark Dalrymple on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct suifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
